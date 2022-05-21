638eeffb3fb05ce72547b42a5341be405e8ca068
ali fani <alifani1377@gmail.com>
2022-05-21 15:38:38 +0430

    first commit

---
+++ New folder/New folder/WebApplication1/Models/users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class users
+    {
+        public users()
+        {
+            Fname = "Ali";
+            Lname = "Fani";
+            gensiat = false;
+            status = false;
+            email = "alifani1377@gmail.com";
+            tel = 123455;
+        }
+        public string aboutme { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public bool gensiat { get; set; }
+        public bool status { get; set; }
+        public string email { get; set; }
+        public int tel { get; set; }
+        public bool ischecked { get; set; }
+    }
+
+}
---
+++ New folder/New folder/WebApplication1/controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            users us = new users();
+            us.aboutme = "<p>my adress : iran.mashhad</p></br>";
+            us.Fname = "Ali";
+            us.Lname = "Fani";
+            us.gensiat = false;
+            us.status = false;
+            us.email = "alifani1377@gmail.com";
+            us.tel = 0933400;
+            us.ischecked = true;
+            
+            
+              return  View(us);
+        }
+
+
+            public ActionResult Contact()
+            {
+            return View();
+            }
+
+
+    }
+}
